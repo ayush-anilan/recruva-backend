d60045bbdcc07123677e163178b0138513e876f9
ayush-anilan <ayushanilan369@gmail.com>
2026-09-07 14:16:44 +0530

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/recruva/RecruvaApplication.java
@@ -0,0 +1,13 @@
+package com.recruva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruva/RecruvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
